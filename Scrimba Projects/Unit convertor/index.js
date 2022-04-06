ac427510db2be7d9289f9e8b809dd6d75b31aeb4
Shwoopde <rduarte@q90.com>
2022-04-06 16:21:46 -0400

    Updating to a working file

--- Scrimba Projects/Unit convertor/index.js
+++ Scrimba Projects/Unit convertor/index.js
@@ -1,51 +1,49 @@
 
 
-let num1 = 12;
-document.getElementById("num-to-convert").textContent = num1;
-console.log(num1);
+// let num1 = 12;
+// document.getElementById("num-to-convert").textContent = num1;
+// console.log(num1);
 
-
-document.getElementById("myDiv").onclick = function() {
-    document.getElementById("myForm").submit();
-  };
 
 function convertToFeet() {
     let result = num1 * 3.28084;
     let n = result.toFixed(3);
-    document.getElementById("feet").textContent = n;
+    document.getElementById("feet").innerHTML = n;
 }
 
 function convertToMeters() {
     let result = num1 * 0.3048;
     let n = result.toFixed(3);
-    document.getElementById("meter").textContent = n;
+    document.getElementById("meter").innerHTML = n;
 }
 
 function convertToGallons() {
     let result = num1 * 0.264172;
     let n = result.toFixed(3);
-    document.getElementById("gallons").textContent = n;
+    document.getElementById("gallons").innerHTML = n;
 }
 
 function converToLiters() {
     let result = num1 * 3.78541;
     let n = result.toFixed(3);
-    document.getElementById("liters").textContent = n;
+    document.getElementById("liters").innerHTML = n;
 }
 
 function convertToKilograms() {
     let result = num1 * 0.453592;
     let n = result.toFixed(3);
-    document.getElementById("kilograms").textContent = n;
+    document.getElementById("kilograms").innerHTML = n;
 }
 
 function convertToPounds() {
     let result = num1 * 2.20462;
     let n = result.toFixed(3);
-    document.getElementById("pounds").textContent = n;
+    document.getElementById("pounds").innerHTML = n;
 }
 
 function allConverters() {
+    console.log("Converting...");
+    num1 = document.getElementById("num-to-convert").value;
     convertToFeet();
     convertToMeters();
     convertToGallons();
@@ -55,12 +53,12 @@ function allConverters() {
 }
 
 function passNumber(){
-    document.getElementById("feet-source").textContent = num1;
-    document.getElementById("meter-source").textContent = num1;
-    document.getElementById("gallons-source").textContent = num1;
-    document.getElementById("liters-source").textContent = num1;
-    document.getElementById("kilograms-source").textContent = num1;
-    document.getElementById("pounds-source").textContent = num1;
+    document.getElementById("feet-source").innerHTML = num1;
+    document.getElementById("meter-source").innerHTML = num1;
+    document.getElementById("gallons-source").innerHTML = num1;
+    document.getElementById("liters-source").innerHTML = num1;
+    document.getElementById("kilograms-source").innerHTML = num1;
+    document.getElementById("pounds-source").innerHTML = num1;
 }
 
 

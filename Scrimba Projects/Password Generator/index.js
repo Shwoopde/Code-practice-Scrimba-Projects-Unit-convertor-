f8519a7c5a58437e927eea554b1ac96de2237adf
Shwoopde <rduarte@q90.com>
2022-04-13 10:06:20 -0400

    Finished project

--- Scrimba Projects/Password Generator/index.js
+++ Scrimba Projects/Password Generator/index.js
@@ -2,7 +2,7 @@ let passwordArray = [
     "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", 
     "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", 
     "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", 
-    "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "{", "[", "}", "]", "|", ":", ";", "\"", "'", ",", "<", ".", ">", "/", "?", "~", "`"
+    "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "{", "[", "}", "]", "|", ":", ";", "\"", "'", ",", ".", "/", "?", "~", "`"
 ];
 
 
@@ -37,15 +37,13 @@ function createPasswords(){
         let randomIndex = Math.floor(Math.random() * passwordArray.length);
         password20char += passwordArray[randomIndex];
     }
-    password8.innerHTML = password8char;
-    password12.innerHTML = password12char;
-    password16.innerHTML = password16char;
-    password20.innerHTML = password20char;
-
-    copyPassword();
-
-    
+    password8.value = password8char;
+    password12.value = password12char;
+    password16.value = password16char;
+    password20.value = password20char;
 }
+
+
 
 function render() {
     password8char = "";
@@ -58,30 +56,35 @@ function passwordProcess(){
     button.addEventListener("click", render);
 }
 
-// function copyPassword(){
-//     let copy8 = document.getElementById("copy-8");
-//     let copy12 = document.getElementById("copy-12");
-//     let copy16 = document.getElementById("copy-16");
-//     let copy20 = document.getElementById("copy-20");
 
-//     copy8.addEventListener("click", function(){
-//         password8.select();
-//         document.execCommand("copy");
-//     });
-//     copy12.addEventListener("click", function(){
-//         password12.select();
-//         document.execCommand("copy");
-//     });
-//     copy16.addEventListener("click", function(){
-//         password16.select();
-//         document.execCommand("copy");
-//     });
-//     copy20.addEventListener("click", function(){
-//         password20.select();
-//         document.execCommand("copy");
-//     });
-// }
-
+function copyToClipBoard1() {
+    if (password8char !== "8 Characters") {
+        password8.select();
+        document.execCommand("copy");
+        alert("Password copied to clipboard");
+    }
+}
+function copyToClipBoard2() {
+    if (password12char !== "12 Characters") {
+        password12.select();
+        document.execCommand("copy");
+        alert("Password copied to clipboard");
+    }
+}
+function copyToClipBoard3() {
+    if (password16char !== "16 Characters") {
+        password16.select();
+        document.execCommand("copy");
+        alert("Password copied to clipboard");
+    }
+}
+function copyToClipBoard4() {
+    if (password20char !== "20 Characters") {
+        password20.select();
+        document.execCommand("copy");
+        alert("Password copied to clipboard");
+    }
+}
 
 
 
